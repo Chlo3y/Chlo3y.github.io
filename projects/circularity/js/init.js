5401b3988c51608bbb45f1c44691b1b41f8ac9f4
Chloe Walker <chloe.walker@jpschools.org>
2024-04-25 19:00:19 +0000

    refactor 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,7 +32,7 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-    for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+    for (var loopsCompleted = 0; loopsCompleted <= 100; loopsCompleted++) {
             drawCircle()
     }
 
