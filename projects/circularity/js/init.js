24330491f33a149089a8cd79d3918946c7a6a324
Chloe Walker <chloe.walker@jpschools.org>
2024-04-25 17:40:14 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -36,11 +36,7 @@ var init = function (window) {
             drawCircle()
     }
 
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
+ 
 
 
         ////////////////////////////////////////////////////////////
@@ -60,7 +56,7 @@ var init = function (window) {
               }
              }
 
-             
+    }
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
@@ -70,7 +66,7 @@ var init = function (window) {
             // TODO 9 : Iterate over the array
            
             
-        }
+        
     
         /* 
         This Function should check the position of a circle that is passed to the 
